23db34e89485193a6b889d22ddb7fbe18cddc79f
gonyalo <joaogoncalo.borges@gmail.com>
2019-11-30 15:54:23 +0000

    permissions.tf

---
+++ permissions.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_group_policy_attachment" "dev-password-change"{
+  group = aws_iam_group.dev.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+}
+
+resource "aws_iam_group_policy_attachment" "dev-iam-read" {
+  group = aws_iam_group.dev.name
+  policy_arn = "arn:aws:iam:aws:policy/IAMReadOnlyAccess"
+}
